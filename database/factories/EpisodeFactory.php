0b192c0e98951b9faf3784b23eb509ea43bea57d
Farid <amoussa.farid2@gmail.com>
2021-01-18 10:41:58 +0100

    Mise en place des données dans la db

---
+++ database/factories/EpisodeFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use App\Models\Episode;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EpisodeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Episode::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(3, true),
+            'video_url' => 'ma_video.com/'. rand(0, 255),
+            'course_id' => Course::all()->random()->id
+        ];
+    }
+}
